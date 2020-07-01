7308df292b1f594f489f7cd9c01c17803fed5809
filleben <filleryben@gmail.com>
2020-07-01 03:18:26 +0100

    updated index function to display_definitions

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
 @app.route('/')
-def index():
-    return render_template('base.html')
+def display_definitions():
+    a_f = mongo.db.definitions.find({"category_name": "A-F"})
+    g_l = mongo.db.definitions.find({"category_name": "G-L"})
+    m_r = mongo.db.definitions.find({"category_name": "M-R"})
+    s_z = mongo.db.definitions.find({"category_name": "S-Z"})
+    return render_template('base.html', A_F=a_f, G_L=g_l, M_R=m_r, S_Z=s_z)
     
 if __name__ == '__main__':
    app.run(host=os.environ.get('IP'),
